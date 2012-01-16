1c0c453dd3b9f3572a23f1f224d4cf3badede5bb
six <six@obsoletetechnology.org>
2012-01-15 23:40:37 -0800

    cleanup

--- IcyBee/IcyBee/AppDelegate.m
+++ IcyBee/IcyBee/AppDelegate.m
@@ -112,16 +112,13 @@
  Returns the managed object context for the application.
  If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
  */
-- (NSManagedObjectContext *)managedObjectContext
-{
-  if (__managedObjectContext != nil)
-  {
+- (NSManagedObjectContext *)managedObjectContext {
+  if (__managedObjectContext != nil) {
     return __managedObjectContext;
   }
   
   NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-  if (coordinator != nil)
-  {
+  if (coordinator != nil) {
     __managedObjectContext = [[NSManagedObjectContext alloc] init];
     [__managedObjectContext setPersistentStoreCoordinator:coordinator];
   }
@@ -132,10 +129,8 @@
  Returns the managed object model for the application.
  If the model doesn't already exist, it is created from the application's model.
  */
-- (NSManagedObjectModel *)managedObjectModel
-{
-  if (__managedObjectModel != nil)
-  {
+- (NSManagedObjectModel *)managedObjectModel {
+  if (__managedObjectModel != nil) {
     return __managedObjectModel;
   }
   NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"ChatModel" withExtension:@"momd"];
@@ -148,8 +143,7 @@
  If the coordinator doesn't already exist, it is created and the application's store added to it.
  */
 - (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
-  if (__persistentStoreCoordinator != nil)
-  {
+  if (__persistentStoreCoordinator != nil) {
     return __persistentStoreCoordinator;
   }
   
@@ -157,8 +151,7 @@
   
   NSError *error = nil;
   __persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-  if (![__persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error])
-  {
+  if (![__persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
     /*
      Replace this implementation with code to handle the error appropriately.
      
@@ -194,8 +187,7 @@
 /**
  Returns the URL to the application's Documents directory.
  */
-- (NSURL *)applicationDocumentsDirectory
-{
+- (NSURL *)applicationDocumentsDirectory {
   return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
 }
 
