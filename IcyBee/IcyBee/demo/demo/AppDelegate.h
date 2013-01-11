2915d2295990dcb88590bec17701c535da4b1623
six <scirocco6@gmail.com>
2013-01-10 20:09:00 -0800

    BAckground image for the new long iPhone

---
+++ IcyBee/IcyBee/demo/demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by Michelle Six on 1/10/13.
+//  Copyright (c) 2013 Michelle Six. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
